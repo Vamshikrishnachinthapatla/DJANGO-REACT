0a0594a4b8e2e40ebe00ac8f89f60b7e6c838ee7
vamshikrishna <krishnachvamshi@gmail.com>
2026-04-21 11:41:25 +0530

    finished-Product

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,13 +31,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-REST_FREAMEWORK = {
+REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES":(
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES":[
         "rest_framework.permissions.IsAuthenticated",
     ],
+    
 }
 
 SIMPLE_JWT = {
